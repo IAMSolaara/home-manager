93f9ec3d5180df3754ed0fe039b4650e8fcabedb
Sarah Evermore <sarah.evermore.02@gmail.com>
2025-03-31 12:24:55 +0200

    add kubetools module :3

--- modules/default.nix
+++ modules/default.nix
@@ -6,5 +6,6 @@
 }: {
   imports = [
     ./nushell
+    ./kubetools
   ];
 }
---
+++ modules/kubetools/default.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.solaaradotnet.pkgsets.kubetools;
+in {
+  options = {
+    solaaradotnet.pkgsets.kubetools = {
+      enable = lib.mkEnableOption "enable kubetools pkgset.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      # k8s tools
+      pkgs.kubectl
+      pkgs.kubecolor
+      pkgs.kubectx
+      pkgs.kustomize
+      pkgs.krew
+      pkgs.k9s
+      pkgs.kubernetes-helm
+      pkgs.cilium-cli
+      pkgs.talosctl
+      pkgs.kubevirt
+    ];
+  };
+}

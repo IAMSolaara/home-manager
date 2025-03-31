7c238d50763964a4e80cd1f428214bbb82b7f45b
Sarah Evermore <sarah.evermore.02@gmail.com>
2025-03-31 11:04:06 +0200

    attempt #2 :3

--- modules/nushell/default.nix
+++ modules/nushell/default.nix
@@ -1,11 +1,23 @@
 {
   pkgs,
   config,
-}: {
-  programs.nushell.enable = true;
-  programs.nushell.configFile.source = ./config.nu;
-  programs.nushell.envFile.source = ./env.nu;
+  lib,
+  ...
+}: let
+  cfg = config.solaaradotnet.shells.nushell;
+in {
+  options = {
+    solaaradotnet.shells.nushell = {
+      enable = lib.mkEnableOption "enable nushell.";
+    };
+  };
 
-  programs.carapace.enable = true;
-  programs.carapace.enableNushellIntegration = true;
+  config = lib.mkIf cfg.enable {
+    programs.nushell.enable = true;
+    programs.nushell.configFile.source = ./config.nu;
+    programs.nushell.envFile.source = ./env.nu;
+
+    programs.carapace.enable = true;
+    programs.carapace.enableNushellIntegration = true;
+  };
 }

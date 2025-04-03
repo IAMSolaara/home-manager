511e80b6ca9f4218ae09914d3fd75600bcdcb152
Sarah Evermore <sarah.evermore.02@gmail.com>
2025-04-03 10:56:58 +0200

    move modules to home.nix :3

--- flake.nix
+++ flake.nix
@@ -41,23 +41,19 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        packages.nixDarwinHomeConfigurations."evermore" =
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.evermore = import ./home.nix;
-            modules = [./modules];
-            extraSpecialArgs = {
-              inherit inputs;
-              inherit system;
-            };
+        packages.nixDarwinHomeConfigurations."evermore" = home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.evermore = import ./home.nix;
+          extraSpecialArgs = {
+            inherit inputs;
+            inherit system;
           };
+        };
         packages.homeConfigurations."evermore" = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
             ./home.nix
-            ./modules
           ];
           extraSpecialArgs = {
             inherit inputs;
--- home.nix
+++ home.nix
@@ -4,6 +4,9 @@
   lib,
   ...
 }: {
+  imports = [
+    ./modules
+  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "evermore";

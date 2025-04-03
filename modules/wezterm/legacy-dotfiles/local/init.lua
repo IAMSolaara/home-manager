bf14a2f9366be8a18b0e026b7dd11d3407185f9d
Sarah Evermore <sarah.evermore.02@gmail.com>
2025-04-03 15:29:48 +0200

    fix :3

---
+++ modules/wezterm/legacy-dotfiles/local/init.lua
@@ -0,0 +1,8 @@
+local config = {}
+-- add local configuration here!
+
+-- useful settings I might want
+--
+-- set shell manually
+-- config.default_prog = { "/Users/evermore/.cargo/bin/nu" }
+return config

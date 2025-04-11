bbc2c8ea9de927cff80ef1fcb5d39fa491bc6d0d
Sarah Evermore <sarah.evermore.02@gmail.com>
2025-04-11 09:52:50 +0200

    neovim config :3

---
+++ modules/editors/neovim/default.nix
@@ -0,0 +1,33 @@
+{
+  pkgs,
+  lib,
+  std,
+  config,
+  options,
+  ...
+}: let
+  cfg = config.solaaradotnet.editors.neovim;
+
+  are_guipkgs_enabled = config.solaaradotnet.pkgsets.guipkgs.enable;
+in {
+  options = {
+    solaaradotnet.editors.neovim = {
+      enable = lib.mkEnableOption "enable neovim.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.neovim.enable = true;
+
+    programs.neovide = lib.mkIf are_guipkgs_enabled {
+      enable = true;
+      settings = {
+        font = {
+          normal = "DejaVuSansM Nerd Font";
+          size = 14.0;
+        };
+        tabs = false;
+      };
+    };
+  };
+}

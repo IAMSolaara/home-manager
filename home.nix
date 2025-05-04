0358d27f5ab5243decab1c63e3a6eaf1d06390e0
Solaara Evermore <evermore@solaara.net>
2025-05-04 13:36:52 +0200

    move stuff out of the way :3

--- home.nix
+++ home.nix
@@ -16,15 +16,6 @@
   solaaradotnet.pkgsets.kubetools.enable = true;
   solaaradotnet.pkgsets.kubetools.flavor = "full";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
   home.packages = lib.mkMerge [
     # Universal packages
     [
@@ -64,7 +55,18 @@
     )
   ];
 
+
+  # --- DO NOT TOCH ---
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 }
